8900132471cda3b35739031c16e1eb8ef62a1a0b
kskdev <kosumikedev@gmail.com>
2018-04-06 21:48:11 +0900

    tag name must be lower case

--- chainerV2/build.sh
+++ chainerV2/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-nvidia-docker build -t osm/chainerV2 .
+nvidia-docker build -t osm/chainerv2 .
 
-nvidia-docker run --rm -it -v /data/osumi/:/home/ osm/chainerV2
+nvidia-docker run --rm -it -v /data/osumi/:/home/ osm/chainerv2
 
 
